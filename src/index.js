84e7bf245be5c858389cbdee2929ca4ac3a0bfe4
saniok017 <saniok017@gmail.com>
2018-10-08 14:41:14 +0300

    longest-consecutive-sequence/10thTask: complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  let sequence = 1, 
+      maxSequence = 0,
+      tempArray = array;
+  const length = tempArray.length;
+
+  if (length < 2) {
+    return length;
+  }
+  if (length > 1){
+    tempArray.sort( (a,b)=> a-b );
+  }
+  for (let c = 0; c < length; c++) {
+    if (tempArray[c]+1 == tempArray[c+1]) {
+      sequence++;
+    }
+    if (tempArray[c] === tempArray[c+1]) {
+      continue;
+    }
+    if (sequence > maxSequence) {
+      maxSequence = sequence;
+    }
+    if (tempArray[c]+1 !== tempArray[c+1]) {
+      sequence = 1;
+    }
+  }
+  return maxSequence;
 }
